8408d2efd42d8dd89b43f9f601214afca8a0d361
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-21 00:00:39 +0300

    Moving logic into errors

--- auth/src/error/database-connection-error.ts
+++ auth/src/error/database-connection-error.ts
@@ -1,6 +1,6 @@
-import { ValidationError } from "express-validator";
 
 export class DatabaseConnectionError extends Error {
+  statusCode = 500
 reason = "Error connecting to Database"
 
   constructor() {
@@ -8,5 +8,9 @@ reason = "Error connecting to Database"
 
     // Only because we are extending a built-in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{message: this.reason}]
   }
 }
