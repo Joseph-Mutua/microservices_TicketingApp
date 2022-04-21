1fee435fde333ccd8187fac4b7d2904e7b71e331
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-21 18:22:20 +0300

    User creation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
@@ -12,9 +12,9 @@ router.post(
     body("password")
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage("Password must be bewtween 4 and 20 characters"),
+      .withMessage("Password must be between 4 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -23,11 +23,17 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log("Creating a user...");
+    const existingUser = await User.findOne({ email });
 
-    throw new DatabaseConnectionError();
+    if (existingUser) {
+      console.log("Email in use");
+      return res.send({});
+    }
 
-    res.send({});
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
