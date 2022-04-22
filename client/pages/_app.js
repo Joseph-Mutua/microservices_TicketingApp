9990afe230ac91c66c2a40d5abe23039089db5b0
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-22 19:17:18 +0300

    Using the userequest hooks

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,36 +1,5 @@
-import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
-export default () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-
-    console.log(email, password);
-  };
-
-  return (
-    <form onSubmit={onSubmit}>
-      <h1>Sign Up</h1>
-      <div className="form-group">
-        <label>Email Address</label>
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="form-control"
-        />
-      </div>
-      <div className="form-group">
-        <label>Password</label>
-        <input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          className="form-control"
-        />
-      </div>
-      <button className="btn btn-primary">Sign Up</button>
-    </form>
-  );
+export default ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
 };
