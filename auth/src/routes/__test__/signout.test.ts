a29ed66796589095f689d1cd8b0c6dfbd0578a08
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-22 13:14:24 +0300

    Add sign out test

--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -1,35 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 
-it("fails when a email that does not exist is supplied", async () => {
-  await request(app)
-    .post("/api/users/signin")
-    .send({
-      email: "test@test.com",
-      password: "password",
-    })
-    .expect(400);
-});
-
-it("fails when an incorrect password is supplied", async () => {
-  await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@test.com",
-      password: "password",
-    })
-    .expect(201);
-
-  await request(app)
-    .post("/api/users/signin")
-    .send({
-      email: "test@test.com",
-      password: "aslkdfjalskdfj",
-    })
-    .expect(400);
-});
-
-it("responds with a cookie when given valid credentials", async () => {
+it("clears the cookie after signing out", async () => {
   await request(app)
     .post("/api/users/signup")
     .send({
@@ -39,12 +11,11 @@ it("responds with a cookie when given valid credentials", async () => {
     .expect(201);
 
   const response = await request(app)
-    .post("/api/users/signin")
-    .send({
-      email: "test@test.com",
-      password: "password",
-    })
+    .post("/api/users/signout")
+    .send({})
     .expect(200);
 
-  expect(response.get("Set-Cookie")).toBeDefined();
+  expect(response.get("Set-Cookie")[0]).toEqual(
+    "session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly"
+  );
 });
