7b64ad1a2766cea655489c8794738847f70b0304
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-23 13:47:04 +0300

    Updating import statements

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { currentUser } from "../middlewares/current-user";
+import { currentUser } from "@mutua-ticketing-service/common";
 
 const router = express.Router();
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,9 +2,12 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 
-import { validateRequest } from "../middlewares/validate-request";
+import {
+  validateRequest,
+  BadRequestError,
+} from "@mutua-ticketing-service/common";
 import { User } from "../models/user";
-import { BadRequestError } from "../errors/bad-request-error";
+
 
 const router = express.Router();
 
